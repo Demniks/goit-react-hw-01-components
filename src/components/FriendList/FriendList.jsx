55228099d2426d580660cc011a6bba8e7cd73e5c
Демченко Сергей <Demniks@ukr.net>
2023-03-19 19:40:16 +0200

    update task 3

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,28 +1,26 @@
 import PropTypes from 'prop-types';
+import { FriendListItem } from '../FriendList/FriendListItem';
 import css from '../FriendList/FriendList.module.css';
 
 export const FriendList = ({ friends }) => {
   return (
-    <ul className={css.friend_list}>
-      {friends.map(({ avatar, name, isOnline, id }) => (
-        <li key={id} className={css.item}>
-          {/* <span className={css.status}></span> */}
-          <span className={isOnline ? css.statusOn : css.statusOff}></span>
-          <img className={css.avatar} src={avatar} alt={name} width="48" />
-          <p className={name}>{name}</p>
-        </li>
+    <ul className={css.friendList}>
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          name={friend.name}
+          isOnline={friend.isOnline}
+          avatar={friend.avatar}
+        />
       ))}
     </ul>
   );
 };
 
-FriendList.protoType = {
+FriendList.propTypes = {
   friends: PropTypes.arrayOf(
-    PropTypes.exact({
-      id: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool.isRequired,
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
     })
   ),
 };
